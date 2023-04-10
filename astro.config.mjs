e8a6e8e11b1c8c7ccee42137b083b0fe578c4073
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2023-04-10 23:11:35 +0530

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://piyush-guptaa.github.io/',
+  base: '/my-repo',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
