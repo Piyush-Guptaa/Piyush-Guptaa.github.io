e48c56c8a97af4500c1b2aad3955a50a68824d48
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2023-04-24 02:19:17 +0530

    Update increment.ts

--- src/utils/increment.ts
+++ src/utils/increment.ts
@@ -1,5 +1,6 @@
 async function incrementCount() {
   const response = await fetch('https://api.countapi.xyz/hit/namespace/piyushgupta.me');
   const data = await response.json();
-  console.log(data.value);
+  const countSpan = document.getElementById('count');
+  countSpan.textContent = data.value;
 }
