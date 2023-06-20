efe2d34146d3c9aed36a4bfe56aba5d736e31c21
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2023-06-20 22:23:34 +0530

    Update seo meta

--- src/utils/increment.ts
+++ src/utils/increment.ts
@@ -1,6 +1,6 @@
-async function incrementCount() {
-  const response = await fetch('https://api.countapi.xyz/hit/namespace/piyushgupta.me');
-  const data = await response.json();
-  const countSpan = document.getElementById('count');
-  countSpan.textContent = data.value;
-}
+// async function incrementCount() {
+//   const response = await fetch('https://api.countapi.xyz/hit/namespace/piyushgupta.me');
+//   const data = await response.json();
+//   const countSpan = document.getElementById('count');
+//   countSpan.textContent = data.value;
+// }
